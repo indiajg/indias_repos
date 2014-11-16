9c095a23d29c6f7e9375ff47c8f47fc03c824804
indiajg <india.glennon@gmail.com>
2014-11-15 17:02:38 -0800

    1

---
+++ project 5/myscript.js
@@ -0,0 +1,105 @@
+/* JavaScript Foundations: Variables */
+
+var color = "red";
+
+var myDiv = document.getElementById('myDiv');
+myDiv.style.background = "black";
+myDiv.style.color = "#ffffff";
+
+// Starts with A-Z a-z _ $
+// Continue with A-Z a-z 0-9 - $
+
+//valid Variables 
+var car = "Toyota";
+var Color = "blue";
+var _myVariable = "something";
+var $specialName = 1;
+var a58389 = "What is this";
+
+//Invalid Variable Names
+// var 3colors = "red blue green";
+//var winning% = 30;
+//var person-name = "Jim";
+//var @you = "are awesome";
+
+// var continue = true;
+
+
+
+var myVar; 
+
+//undefined = true;
+
+console.log(typeof myVar === "undefined");
+console.log(myVar === "undefined");
+
+var x = null;
+
+
+if (myVar == null) {
+	console.log("if")
+} else {
+	console.log("Else");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
